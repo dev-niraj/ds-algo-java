49ce1aef25883ca16591c2662168b617b66dd7ad
dev-niraj <think2secure@gmail.com>
2020-06-13 23:21:23 +0530

    quick sort added

--- src/com/dsalgo/QuickSort.java
+++ src/com/dsalgo/QuickSort.java
@@ -1,5 +1,31 @@
 package com.dsalgo;
 
 public class QuickSort {
+    public void quickSort(int[] nums, int start, int end) {
+        if (start >= end)
+            return;
 
+        var boundary = partition(nums, start, end);
+        quickSort(nums, start, boundary - 1);
+        quickSort(nums, boundary + 1, end);
+    }
+
+    private int partition(int[] nums, int start, int end) {
+        var pivot = nums[end];
+        var boundary = start - 1;
+
+        for(var i = start; i <= end; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, i, ++boundary);
+            }
+        }
+
+        return boundary;
+    }
+
+    private void swap(int[] arr, int first, int second) {
+        int temp  = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }

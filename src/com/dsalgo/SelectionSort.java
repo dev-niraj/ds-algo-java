88eb71594a0ff06649b962bf034c965b130945cb
dev-niraj <think2secure@gmail.com>
2020-06-14 11:27:21 +0530

    Selection sort Code added

---
+++ src/com/dsalgo/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.dsalgo;
+
+public class SelectionSort {
+    public void sort(int[] array) {
+        int lastIndex = array.length - 1;
+
+        for(int i = lastIndex; i >= 1; i--) {
+            int indexOfItemToPlace = 0;
+            for(int j = 1; j <= i; j++) {
+                if (array[j] > array[indexOfItemToPlace]) {
+                    indexOfItemToPlace = j;
+                }
+            }
+            swap(array, indexOfItemToPlace, i);
+        }
+    }
+
+    private void findMinIndex() {
+
+    }
+
+    private void swap(int[] arr, int first, int second) {
+        int temp  = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}

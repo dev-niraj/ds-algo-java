1220d2eecf966ce4d012c6f9f148c7342457018b
dev-niraj <think2secure@gmail.com>
2020-06-15 18:14:54 +0530

    stack implementation from scratch implemented

---
+++ src/com/dsalgo/Stack.java
@@ -0,0 +1,43 @@
+package com.dsalgo;
+
+import java.util.Arrays;
+
+public class Stack {
+    private final int[] items;
+    private int count;
+
+    public Stack (int size) {
+        items = new int[size];
+    }
+
+    public void push(int item) {
+        if (count == items.length)
+            throw new StackOverflowError();
+
+        items[count++] = item;
+    }
+
+    public int pop() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        return items[--count];
+    }
+
+    public int peek() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        return items[count - 1];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public String toString() {
+        var content =  Arrays.copyOfRange(items, 0, count);
+        return Arrays.toString(content);
+    }
+}

5df7c301a42cef375c382a13051e38c24c87999c
dev-niraj <think2secure@gmail.com>
2020-06-17 12:02:35 +0530

    reversed integer and rotate matrix code added

--- src/com/dsalgo/ReverseInteger.java
+++ src/com/dsalgo/ReverseInteger.java
@@ -3,7 +3,6 @@ package com.dsalgo;
 public class ReverseInteger {
     public int reverse(int n) {
         int result = 0;
-        String s = "test";
         while (n != 0) {
             int lastDigit = n % 10;
             int newResult = result * 10 + lastDigit;

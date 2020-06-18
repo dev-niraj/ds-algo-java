ca19a527843884b2e22a4867510e7689097670c1
dev-niraj <think2secure@gmail.com>
2020-06-19 00:17:06 +0530

    Binary Search Iterative and Recursive Code added

---
+++ src/com/dsalgo/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.dsalgo;
+
+public class BinarySearch {
+    public int binarySearchI(int[] nums, int x) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (nums[m] == x)
+                return m;
+
+            if (nums[m] < x)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+
+        return -1;
+    }
+
+    public int binarySearchR(int[] nums, int l, int r, int x) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == x)
+                return mid;
+
+            if (nums[mid] > x)
+                return binarySearchR(nums, l, mid - 1, x);
+
+            return binarySearchR(nums, mid + 1, r, x);
+        }
+
+        return -1;
+    }
+}
